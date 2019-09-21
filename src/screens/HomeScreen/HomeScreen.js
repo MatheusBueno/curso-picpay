8f2328c0cccfe95e39f4a32acc443b70cb9e0116
Bueno <matheusbueno1063@gmail.com>
2019-09-21 18:08:32 -0300

    Adiciona animação da tela inicial

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, Animated } from 'react-native';
 import { Header } from 'react-navigation-stack';
 import Ripple from 'react-native-material-ripple';
 
-import { SUGGESTED_HEIGHT, USER_SUGGESTIONS, ACTIVITIES } from '.././../config/constants';
+import { SCREEN_HEIGHT, USER_SUGGESTIONS, ACTIVITIES } from '.././../config/constants';
 import { styles } from './styles';
 import HeaderHome from '../../components/HeaderHome/HeaderHome';
 import SuggestedUsers from '../../components/SuggestedUsers/SuggestedUsers';
@@ -11,12 +11,20 @@ import TabHome from '../../components/TabHome/TabHome';
 import theme from '../../styles/theme';
 import UserActivity from '../../components/UserActivity/UserActivity';
 
+const SUGGESTED_HEIGHT = SCREEN_HEIGHT / 3.5;
 const HIDE_HEIGHT = SUGGESTED_HEIGHT + Header.HEIGHT;
 
 export default function HomeScreen() {
+  const [scrollY] = useState(new Animated.Value(0));
+
   const renderAllActivities = () => {
     return (
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <Animated.ScrollView
+        scrollEventThrottle={16}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+          useNativeDriver: true
+        })}
+        showsVerticalScrollIndicator={false}>
         {ACTIVITIES.map(activity => {
           return (
             <Ripple
@@ -29,21 +37,29 @@ export default function HomeScreen() {
             </Ripple>
           );
         })}
-      </ScrollView>
+      </Animated.ScrollView>
     );
   };
 
+  const translateY = scrollY.interpolate({
+    inputRange: [0, HIDE_HEIGHT + 80],
+    outputRange: [0, -HIDE_HEIGHT],
+    extrapolate: 'clamp'
+  });
+
   return (
     <View style={styles.safeArea}>
-      <View>
-        <View style={styles.header}>
-          <HeaderHome />
+      <Animated.View style={{ translateY }}>
+        <View>
+          <View style={styles.header}>
+            <HeaderHome />
+          </View>
+          <SuggestedUsers users={USER_SUGGESTIONS} />
         </View>
-        <SuggestedUsers users={USER_SUGGESTIONS} />
-      </View>
-      <View style={styles.tabsContainer}>
-        <TabHome allActivities={renderAllActivities} />
-      </View>
+        <View style={styles.tabsContainer}>
+          <TabHome allActivities={renderAllActivities} />
+        </View>
+      </Animated.View>
     </View>
   );
 }
