6cc9e6761a235bd7080bcdd65b430e96b816a238
Bueno <matheusbueno1063@gmail.com>
2019-09-21 14:14:19 -0300

    corrige import

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,7 +3,7 @@ import { Dimensions, StatusBar } from 'react-native';
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const STATUSBAR_HEIGHT = StatusBar.currentHeight || 24;
-export const TABS_WIDTH = WIDTH_SCREEN / 5;
+export const TABS_WIDTH = SCREEN_WIDTH / 5;
 
 export const USER_SUGGESTIONS = [
   {
